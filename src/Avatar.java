048b151b0f29202a12a13aeb4775263f4966806e
Sarah Vicchiollo <vicchiollos8832@my.uwstout.edu>
2023-04-27 09:35:51 -0500

    update avatar

--- src/Avatar.java
+++ src/Avatar.java
@@ -1,4 +1,34 @@
 
 public class Avatar {
+	
+	private Gender gender;
+	private Head head;
+	private Eyes eyes;
+	private Nose nose;
+	private Mouth mouth;
+	private Hair hair;
+	
+	public Avatar() {
+		gender = null;
+		head = null;
+		eyes = null;
+		nose = null;
+		mouth = null;
+		hair = null;
+	}
+	
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	
+	public String getGender() {
+		return gender.getText();
+	}
+	
+	public void setHead(Head head) {
+		this.head = head;
+	}
+	
+	
 
 }

9ea427ac30abab79e8c3559beb9752f80ecd8002
Sarah Vicchiollo <vicchiollos8832@my.uwstout.edu>
2023-04-30 18:14:45 -0500

    ye

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
